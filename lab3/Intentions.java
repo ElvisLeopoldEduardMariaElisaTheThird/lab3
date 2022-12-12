0a1ba7b90be8e13686c35c2494b1e9205b5a1dd6
ElvisLeopoldEduardMariaElisaTheThird <67345794+ElvisLeopoldEduardMariaElisaTheThird@users.noreply.github.com>
2022-12-12 14:38:09 +0300

    laba 3 java

---
+++ lab3/Intentions.java
@@ -0,0 +1,20 @@
+package lab3;
+
+import java.util.Objects;
+
+public enum Intentions {
+    LOVE("любит "),
+    HATE("ненавидит "),
+    NONE("безразлтчен к "),
+    ANGER("злится на "),
+    MARRY("хочет пожениться ");
+    Object
+    protected String intent;
+    Intentions(String intent){
+        this.intent=intent;
+    }
+
+    public String getIntent(){
+        return intent;
+    }
+}
---
+++ lab3/Main.java
@@ -0,0 +1,81 @@
+package lab3;
+
+import java.util.Random;
+
+public class Main{
+    public static void main(String[] args){
+        //создание персонажей
+        HumanBeing Malish = new HumanBeing("Малыш", IsAdult.No, Gender.Male);
+        HumanBeing Father = new HumanBeing("Папа", IsAdult.Yes, Gender.Male);
+        HumanBeing Mother = new HumanBeing("Мама", IsAdult.Yes, Gender.Female);
+        HumanBeing Bosse = new HumanBeing("Боссе", IsAdult.No, Gender.Male);
+        HumanBeing Betan = new HumanBeing("Бетан", IsAdult.No, Gender.Male);
+        HumanBeing Karlson = new HumanBeing("Карлсон", IsAdult.No, Gender.Male);
+        HumanBeing Gunilla = new HumanBeing("Гунилла", IsAdult.No, Gender.Female);
+
+        //создание объектов
+        Object Gazeta = new Object("газета", 1);
+        Object Koleni = new Object("колени", 1);
+
+
+        Random random = new Random();
+
+
+        Mother.read(Gazeta);
+        Malish.sit(Koleni);
+
+        int a = random.nextInt(100);
+
+        if (a<75) {
+            Malish.think();
+        } else if (a<50){
+            Malish.read(Gazeta);
+        } else if(a<25){
+            Malish.say(Malish, Phrases.values()[random.nextInt(Phrases.values().length)]);
+        } else{
+            Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Mother, Kinship.MOTHER);
+        }
+
+        Intentions.values()[random.nextInt(Intentions.values().length)].getIntent();
+
+        System.out.println();
+
+        System.out.println("Малыш размышляет над тем кого же он любит");
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Mother, Kinship.MOTHER);
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Father, Kinship.FATHER);
+
+        System.out.println();
+
+        Malish.say(Mother, Phrases.values()[random.nextInt(Phrases.values().length)]);
+        Mother.say(Malish, Phrases.values()[random.nextInt(Phrases.values().length)]);
+
+        System.out.println();
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Bosse, Kinship.BROTHER);
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Betan, Kinship.SISTER);
+
+        System.out.println();
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Bosse, Kinship.BROTHER);
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Betan, Kinship.SISTER);
+
+        System.out.println();
+
+        Malish.say(Mother, Phrases.values()[random.nextInt(Phrases.values().length)]);
+        Mother.say(Malish, Phrases.values()[random.nextInt(Phrases.values().length)]);
+
+        System.out.println();
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Karlson, Kinship.NONE);
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Gunilla, Kinship.NONE);
+
+        Malish.feel(Intentions.values()[random.nextInt(Intentions.values().length)], Intensity.values()[random.nextInt(Intensity.values().length)], Mother, Kinship.MOTHER);
+
+        System.out.println("Как бы Малыш ни хотел жениться на маме, это невозможно");
+
+
+
+    }
+}
